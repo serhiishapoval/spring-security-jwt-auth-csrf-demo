312d712d3f43a2613fbc7156ec9a0e5c6703985a
Serhii Shapoval <acidserg359@gmail.com>
2025-11-20 20:45:35 +0200

    Removed public keyword from declaration of UserDetailsServiceSecure.getUserDetailsMethodLevelSecurity

--- src/main/java/com/security/demo/service/UserDetailsServiceSecure.java
+++ src/main/java/com/security/demo/service/UserDetailsServiceSecure.java
@@ -1,5 +1,5 @@
 package com.security.demo.service;
 
 public interface UserDetailsServiceSecure {
-  public String getUserDetailsMethodLevelSecurity();
+  String getUserDetailsMethodLevelSecurity();
 }
